6426933c9a8b9b265148f0bd5ed090992901c382
Benicio-m <beniciomr36@gmail.com>
2025-11-03 19:11:08 -0300

    Moved some files

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,38 @@
-
 import './App.css'
-
+import play from './assets/play.svg'
+import jpg from "./assets/kandinsky-composicao-viii-d.jpg"
 function App() {
-  return (
-    <div>
-        <button onClick={}>
-            <img src="" alt="" />
-        </button>
-    </div>
-  )
+    return (
+        <div style={{backgroundImage: `url(${jpg})`,  backgroundPosition: 'center',  backgroundSize: 'cover',  backgroundRepeat: 'no-repeat'}}>
+            <button title='Play' style={{position: "absolute", left: "20.5em", top: "5.5em", backgroundColor: "#ff5e00ff", height: "3em", width: "3em"}}>
+                <img src={play} alt='play' style={{transform: "translate(-40%, -9%)"}}/>
+            </button>
+            <button title='Play' style={{position: "absolute", left: "31.5em", top: "5.5em", backgroundColor: "#1900ff", height: "3em", width: "3em"}}>
+                <img src={play} alt='play' style={{transform: "translate(-40%, -9%)"}}/>
+            </button>
+            <button title='Play' style={{position: "absolute", left: "58.5em", top: "5.5em", backgroundColor: "#00ff4cff", height: "3em", width: "3em"}}>
+                <img src={play} alt='play' style={{transform: "translate(-40%, -9%)"}}/>
+            </button>
+            <button title='Play' style={{position: "absolute", left: "42.5em", top: "17.6em", backgroundColor: "#008cff", height: "3em", width: "3em"}}>
+                <img src={play} alt='play' style={{transform: "translate(-40%, -9%)"}}/>
+            </button>
+            <button title='Play' style={{position: "absolute", left: "34.5em", top: "30.7em", backgroundColor: "#ff0800", height: "3em", width: "3em"}}>
+                <img src={play} alt='play' style={{transform: "translate(-40%, -9%)"}}/>
+            </button>
+            <button title='Play' style={{position: "absolute", left: "20.5em", top: "5.5em", backgroundColor: "#ff5e00ff", height: "3em", width: "3em"}}>
+                <img src={play} alt='play' style={{transform: "translate(-40%, -9%)"}}/>
+            </button>
+            <button title='Play' style={{position: "absolute", left: "20.5em", top: "5.5em", backgroundColor: "#ff5e00ff", height: "3em", width: "3em"}}>
+                <img src={play} alt='play' style={{transform: "translate(-40%, -9%)"}}/>
+            </button>
+            <button title='Play' style={{position: "absolute", left: "20.5em", top: "5.5em", backgroundColor: "#ff5e00ff", height: "3em", width: "3em"}}>
+                <img src={play} alt='play' style={{transform: "translate(-40%, -9%)"}}/>
+            </button>
+            <button title='Play' style={{position: "absolute", left: "20.5em", top: "5.5em", backgroundColor: "#ff5e00ff", height: "3em", width: "3em"}}>
+                <img src={play} alt='play' style={{transform: "translate(-40%, -9%)"}}/>
+            </button>
+        </div>
+    )
 }
 
 export default App
